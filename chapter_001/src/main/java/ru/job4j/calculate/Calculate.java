544be9ef0adf9cad0f4761e01f216139e4c9e785
Wangog <inquisitive@bk.ru>
2019-01-11 23:38:10 +0300

    1.3. JavaDoc. Code convention[#99881]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
-
+/**
+*Class Calculate решение задачи 1.3. JavaDoc. Code convention
+*@author shcherbakov
+*@since 11.01.2019
+*@version 1.0
+*/ 
 public class Calculate{
-	
+	/**
+	* Main.
+	*@param args - args
+	*/
 	public static void main(String[]args){
 		System.out.println("Hello World");
 	}
