e17a22950f1c8903508f68f9e27eb48e88b456bd
Wangog <inquisitive@bk.ru>
2019-02-02 22:42:19 +0300

    3.1. Элементарный калькулятор.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,21 +3,24 @@ package ru.job4j.calculate;
 *Class Calculate решение задачи 3.1 Элементарный клькулятор
 *@author shcherbakov
 *@since 02.02.2019
-*@version 1.0
+*@version 1.0.1
 */ 
 public class Calculate {
 	private double result;
 	/**
 	 * Описываем аргументы first и second.
-	 * Опыисываем сложние аргументов.
+	 * Опыисываем действия аргументов.
 	 */
-	public void add(double first,double second){
-		this.result=first+second;
-	}
+	public void add(double first, double second) {
+		this.result = first + second;
+		this.result = first - second;
+		this.result = first * second;
+		this.result = first / second;
+    }
 	/**
 	*Выводим полученный результат.
 	*/
-	public double getResult(){
+	public double getResult() {
 		return this.result;
 	}
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 *Class CalculateTest решение задачи 3.1. Элементарный калькулятор
 *@author shcherbakov
 *@since 02.02.2019
-*@version 1.0
+*@version 1.0.1
 */ 
 public class CalculateTest {
 	/**
@@ -17,10 +17,43 @@ public class CalculateTest {
 	@Test
 	public void whenAddOnePlusOneThenTwo() {
 		Calculate calc = new Calculate();
-		calc.add(0,2);
-		double result=calc.getResult();
-		double expected=2;
-		assertThat(result,is(expected));
+		calc.add(1, 1);
+		double result = calc.getResult();
+		double expected = 1;
+		assertThat(result, is(expected));
+	}
+	/**
+	 * Test вычитание.
+	 */
+	@Test
+	public void whenTwoSubtractOneThenOne() {
+		Calculate calc = new Calculate();
+		calc.add(2, 1);
+		double result = calc.getResult();
+		double expected = 2;
+		assertThat(result, is(expected));
+	}
+	/**
+	 * Test умножение.
+	 */
+	@Test
+	public void whenMultipleTwoOnTwoThenOne() {
+		Calculate calc = new Calculate();
+		calc.add(2, 2);
+		double result = calc.getResult();
+		double expected = 1;
+		assertThat(result, is(expected));
+	}
+	/**
+	 * Test деление.
+	 */
+	@Test
+	public void whenDivTwoOnTwoThenOne() {
+		Calculate calc = new Calculate();
+		calc.add(2, 2);
+		double result = calc.getResult();
+		double expected = 1;
+		assertThat(result, is(expected));
 	}
 
 }
