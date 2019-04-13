380be01efb122114cd232c8e431baf1db669c336
Wangog <inquisitive@bk.ru>
2019-04-13 13:52:08 +0300

    4.4. Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,14 +2,28 @@ package ru.job4j.condition;
 /**
  *Class Max решение задачи 4.4. Максимм из трех чисел
  *@author shcherbakov
- *@since 12.04.2019
+ *@since 13.04.2019
  *@version 1.0.0
  */
-
 public class MultiMax {
+    /**
+     * Максимальное из двух чисел.
+     *
+     * @param first  первое число.
+     * @param second второе число.
+     * @return значение больше меньше или равно.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+    /**
+     * Максимальное из трех чисел.
+     * @param first первое число.
+     * @param second второе число.
+     * @param third третье число.
+     * @return наибольшее из трех чисел.
+     */
     public int max(int first, int second, int third) {
-        int result = first;
-        // if ...
-        return result;
+        return max(max(first, second), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -6,15 +6,27 @@ import static org.junit.Assert.assertThat;
 /**
  *Class Max решение задачи 4.4. Максимм из трех чисел
  *@author shcherbakov
- *@since 12.04.2019
+ *@since 13.04.2019
  *@version 1.0.0
  */
 
 public class MultiMaxTest {
     @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 1, 2);
+        assertThat(result, is(4));
+    }
+    @Test
     public void whenSecondMax() {
         MultiMax check = new MultiMax();
         int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 2, 4);
         assertThat(result, is(4));
     }
 }
