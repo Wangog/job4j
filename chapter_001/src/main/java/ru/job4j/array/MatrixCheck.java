06ce25fa578cbdf34d9a8f9b1e865c9fd4b36c4f
Wangog <inquisitive@bk.ru>
2020-04-08 15:57:56 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+/**
+ *Class MatrixCheck решение задачи 	6.7. Квадратный массив заполнен true или false по диагоналям.
+ *@author shcherbakov
+ *@since 08.04.2020
+ *@version 1.0.0
+ */
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = true;
+        int GD = 0; //Горизонтальная диагональ
+        int VD = 0; //Вертикальная диагональ
+        for (int i = 0; i < board.length - 1; i++) {
+            if (board[i][i] == board[i + 1][i + 1]) {
+                GD++;
+            }
+            if (board[board.length - 1][board.length - 1] == board[board.length - 1 - i][board.length - 1 - i]) {
+                VD++;
+            }
+        }
+            if (GD == board.length - 1 || VD == board.length - 1);
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.array;
+/**
+ *Class MatrixCheckTest решение задачи 	6.7. Квадратный массив заполнен true или false по диагоналям.
+ *@author shcherbakov
+ *@since 08.04.2020
+ *@version 1.0.0
+ */
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataHMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
+    }
+}
