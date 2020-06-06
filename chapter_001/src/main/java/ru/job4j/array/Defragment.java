cef8f6a4fe7d8ed2d6f115afb91c4b3c9bd9ed38
Wangog <inquisitive@bk.ru>
2020-06-07 00:14:24 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+/**
+ *Class Defragment решение задачи 6.8. Удаление дубликатов в массиве.
+ *@author shcherbakov
+ *@since 08.04.2020
+ *@version 1.0.0
+ */
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; // указатель, на не null ячейку.
+                // переместить первую не null ячейку
+                // Здесь нужен цикл while
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
